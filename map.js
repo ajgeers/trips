292b60e077e3ba61bb2bcb9324b7ec2d635d644c
Arjan Geers <ajgeers@gmail.com>
2019-11-10 11:30:32 +0100

    Load data and add to map

--- map.js
+++ map.js
@@ -10,3 +10,66 @@ L.tileLayer(
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiYWpnZWVycyIsImEiOiJjaWs5cHZmMWswZDJ6dmhrdTg3YnNidW44In0.QPU_FxLe89puMPjldiaFCg',
     }).addTo(map);
+
+let geojsonMarkerOptions = {
+    radius: 4,
+    fillColor: "#e41a1c",
+    color: "#fff",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+};
+
+const url = 'https://gist.githubusercontent.com/ajgeers/015e164b166a81c6ae5d4be4fd61b331/raw/3ee159a555e73e3aabcd587f1af1d10eb8738e5d/itinerary.csv';
+
+function filterCSV(row, index, columns) {
+    if (row.latitude !== '') {
+        return row;
+    }
+}
+
+d3.csv(url, filterCSV).then(function(data) {
+
+    let pointsJson = d3.nest()
+        .key(d => d.latitude) // assuming location latitude is unique
+        .rollup(v => ({
+            placename: v[0].placename,
+            country: v[0].country,
+            nights: v.length,
+            latitude: +v[0].latitude,
+            longitude: +v[0].longitude,
+            dates: v.map(e => e.date)
+        }))
+        .entries(data)
+        .map(e => e.value);
+
+    let pointsGeojsonFeatures = [];
+    pointsJson.forEach(function(point) {
+        let feature = {
+            type: 'Feature',
+            properties: {
+                placename: point.placename,
+                country: point.country,
+                nights: point.nights,
+                dates: point.dates
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [point.longitude, point.latitude]
+            }
+        };
+        pointsGeojsonFeatures.push(feature);
+    });
+
+    let pointsGeojson = {
+        type: 'FeatureCollection',
+        features: pointsGeojsonFeatures
+    };
+
+    L.geoJSON(pointsGeojson, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+
+});
